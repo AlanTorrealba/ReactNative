e7db19327a16ebb0c5f4fdf239fdc0a279927c3c
ant <antorrealbatapia@gamil.com>
2024-08-20 22:34:38 -0400

    buttons react native paper view

--- App.tsx
+++ App.tsx
@@ -1,15 +1,15 @@
-import { View, Text, SafeAreaView } from "react-native"
-import { HellowWorldScreen } from './src/presentation/screens/HellowWorldScreen';
-import { CounterScreen } from "./src/presentation/screens/CounterScreen";
-
-
+import {View, Text, SafeAreaView} from 'react-native';
+import {HellowWorldScreen} from './src/presentation/screens/HellowWorldScreen';
+import {CounterScreen} from './src/presentation/screens/CounterScreen';
+import {PaperProvider} from 'react-native-paper';
 
 export const App = () => {
   return (
-    <SafeAreaView style={{flex:1}}>
-      {/* <HellowWorldScreen name= 'Alan Noel Torrealba Tapia'/> */}
-      <CounterScreen/>
-    </SafeAreaView>
-  )
-}
-
+    <PaperProvider>
+      <SafeAreaView style={{flex: 1}}>
+        {/* <HellowWorldScreen name= 'Alan Noel Torrealba Tapia'/> */}
+        <CounterScreen />
+      </SafeAreaView>
+    </PaperProvider>
+  );
+};
