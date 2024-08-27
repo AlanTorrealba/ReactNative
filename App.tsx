abe33802ed2c80a4ac4e6eb425c26221da33f281
ant <antorrealbatapia@gamil.com>
2024-08-26 22:51:55 -0400

    Box Container reactnative

--- App.tsx
+++ App.tsx
@@ -1,19 +1,23 @@
-import { SafeAreaView} from 'react-native';
+import {SafeAreaView} from 'react-native';
 import {HellowWorldScreen} from './src/presentation/screens/HellowWorldScreen';
 import {CounterScreen} from './src/presentation/screens/CounterScreen';
 import {CounterM3Screen} from './src/presentation/screens/CounterM3Screen';
 import {PaperProvider} from 'react-native-paper';
 import Ioicon from 'react-native-vector-icons/Ionicons';
+import {BoxObjectModelScreen} from './src/presentation/screens/BoxObjectModelScreen';
+import {DimesionScreen} from './src/presentation/screens/DimesionScreen';
 export const App = () => {
   return (
     <PaperProvider
       settings={{
-        icon: (props) => <Ioicon {...props} />
+        icon: props => <Ioicon {...props} />,
       }}>
       <SafeAreaView style={{flex: 1}}>
         {/* <HellowWorldScreen name= 'Alan Noel Torrealba Tapia'/> */}
         {/* <CounterScreen /> */}
-        <CounterM3Screen />
+        {/* <CounterM3Screen /> */}
+        {/* <BoxObjectModelScreen /> */}
+        <DimesionScreen />
       </SafeAreaView>
     </PaperProvider>
   );
